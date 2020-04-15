03a936334c0b30e7b9b89c87d0a3aaf9c7001f91
KKimj <kkimj@hanyang.ac.kr>
2020-04-15 18:56:09 +0900

    deploy test 0.0.1V.

--- coronasurvivorpackage/lib/main.dart
+++ coronasurvivorpackage/lib/main.dart
@@ -12,16 +12,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Corona Survivor',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        accentColor: const Color(0xFFF16876),
+        primaryColor: const Color(0xFFB3BFE7),
+        fontFamily: 'SCDream',
+        /*textTheme: TextTheme(
+          title: TextStyle(fontSize: 18.0, fontStyle: FontStyle.normal),
+          body1: TextStyle(fontSize: 14.0, fontStyle: FontStyle.normal)
+        )*/
+
       ),
       home: RootPage(),
     );
--- coronasurvivorpackage/lib/mainPage.dart
+++ coronasurvivorpackage/lib/mainPage.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 
+import 'package:csv/csv.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -8,15 +10,78 @@ class MainPage extends StatefulWidget {
   _MainPageState createState() => _MainPageState();
 }
 
+List init_()
+{
+  Response response = init();
+  return CsvToListConverter().convert(response.toString());
+}
+
+dynamic init() async {
+
+  var dio = Dio();
+  Response response = await dio.get('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports_us/04-14-2020.csv');
+
+  return response;
+}
+
 class _MainPageState extends State<MainPage> {
+  List covid_19;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    //covid_19 = init_();
+  }
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(title: Text('Corona Survivor'),),
         drawer: Drawer(),
-        body: Center(
-          child: Image.asset('assets/images/web-usamap.png'),
+        body: SingleChildScrollView(
+          physics: ScrollPhysics(),
+          child: Column(
+            children: <Widget>[
+              Center(child: Text('Live COVID 19 Maps', style: TextStyle( fontWeight: FontWeight.bold, fontSize: 40),), ),
+              Center(
+                child: Image.asset('assets/images/web-usamap.png'),
+              ),
+              covid_19 == null? Center(child: CircularProgressIndicator(),) : ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                itemCount: covid_19.length,
+                  itemBuilder: (BuildContext context, int index) {
+
+                    return index==0? Text('US List view') : Text(index.toString()+'번째 주:'+ covid_19[index][0]+': 확진자수 '+covid_19[index][5].toString());
+                  }
+              ),
+            ],
+          ),
+        ),
+        //coronasurvivorpackage
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.compare_arrows),
+          onPressed: () async
+          {
+            var dio = Dio();
+            Response response = await dio.get('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports_us/04-14-2020.csv');
+            String k = response.toString();
+            List tmp =CsvToListConverter().convert(k);
+
+            int i=0;
+            print(tmp[0]);
+            for(i=1;i<tmp.length;i++)
+              {
+                print(tmp[i][0]+': 확진자수 '+tmp[i][5].toString());
+              }
+
+            setState(() {
+              covid_19 = tmp;
+
+            });
+          }
+          ,
         ),
         ),
       );
